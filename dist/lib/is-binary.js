f75f083a071ef2115f9f234b880feaf99c159bf5
atlmtw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 <masontywong@gmail.com>
2022-07-19 10:58:19 -0400

    use webpack again

--- dist/lib/is-binary.js
+++
@@ -1,7 +0,0 @@
-var equal = require('buffer-equal');
-
-var HEADER = Buffer.from([66, 77, 70, 3]);
-export function isBinaryFormat(buf) {
-  if (typeof buf === 'string') return buf.substring(0, 3) === 'BMF';
-  return buf.length > 4 && equal(buf.slice(0, 4), HEADER);
-}
