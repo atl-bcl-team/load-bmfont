1d434eb6b591c661aff647b62b067d91dfda6442
atlmtw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 <masontywong@gmail.com>
2022-07-19 10:14:34 -0400

    use export and import

--- src/lib/is-binary.js
+++ src/lib/is-binary.js
@@ -1,8 +1,8 @@
 var equal = require('buffer-equal')
 var HEADER = Buffer.from([66, 77, 70, 3])
 
-export default function(buf) {
+export function isBinaryFormat(buf) {
   if (typeof buf === 'string')
     return buf.substring(0, 3) === 'BMF'
-  return buf.length > 4 && equal(buf.slice(0, 4), HEADER)
+    return buf.length > 4 && equal(buf.slice(0, 4), HEADER)
 }
--- test-client.js
+++
@@ -1,4 +0,0 @@
-const load = require('./dist/index');
-load('/fnt/Lato-Regular-32.fnt', (_, font) => {
-    console.log(font);
-})
